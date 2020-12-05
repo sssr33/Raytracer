2bf5949944763039eece4373bc7eeeea91417c9a
Alexander Chaikovsky <sskr33@gmail.com>
2020-12-06 01:43:21 +0200

    [RT-4] ray, background rendering

---
+++ Raytracer/Math/ray.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "vec3.h"
+
+template<class T>
+struct ray
+{
+    ray() = default;
+    ray(const vec3<T>& origin, const vec3<T>& direction)
+        : origin(origin), direction(direction)
+    {}
+
+    vec3<T> pointAtParameter(float t) const
+    {
+        vec3<T> pt = this->origin + t * this->direction;
+    }
+
+    vec3<T> origin;
+    vec3<T> direction;
+};
--- Raytracer/Render/Functor/RayTraceFunctor.cpp
+++ Raytracer/Render/Functor/RayTraceFunctor.cpp
@@ -13,6 +13,11 @@ void RayTraceFunctor::operator()(const MassiveCompute::Block& block)
 {
     const size_t height = image.GetHeight();
 
+    vec3<float> lowerLeftCorner = { -2.f, -1.f, -1.f };
+    vec3<float> horizontal = { 4.f, 0.f, 0.f };
+    vec3<float> vertical = { 0.f, 2.f, 0.f };
+    vec3<float> origin = { 0.f, 0.f, 0.f };
+
     for (size_t yRow = block.top; yRow < block.bottom; yRow++)
     {
         BGRA<uint8_t>* row = image.GetRow(yRow);
@@ -20,12 +25,11 @@ void RayTraceFunctor::operator()(const MassiveCompute::Block& block)
 
         for (size_t x = block.left; x < block.right; x++)
         {
-            vec3<float> col =
-            {
-                static_cast<float>(x) / static_cast<float>(block.imageWidth),
-                static_cast<float>(y) / static_cast<float>(block.imageHeight),
-                0.2f
-            };
+            float u = static_cast<float>(x) / static_cast<float>(block.imageWidth);
+            float v = static_cast<float>(y) / static_cast<float>(block.imageHeight);
+
+            ray<float> r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+            vec3<float> col = this->Color(r);
 
             vec3<float> col8Bit = col * 255.99f;
             BGRA<uint8_t> pixel = {};
@@ -38,4 +42,11 @@ void RayTraceFunctor::operator()(const MassiveCompute::Block& block)
             row[x] = pixel;
         }
     }
+}
+
+vec3<float> RayTraceFunctor::Color(const ray<float>& r) const
+{
+    vec3<float> unitDirection = r.direction.normalized();
+    float t = 0.5f * (unitDirection.y + 1.f);
+    return (1.f - t) * vec3<float>(1.f) + t * vec3<float>(0.5f, 0.7f, 1.f);
 }

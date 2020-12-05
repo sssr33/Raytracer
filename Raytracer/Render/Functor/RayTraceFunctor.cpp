36b103c7daa12c26702ad7016734a3c601f3b713
Alexander Chaikovsky <sskr33@gmail.com>
2020-12-06 01:54:24 +0200

    [RT-4] aspect ratio

--- Raytracer/Render/Functor/RayTraceFunctor.cpp
+++ Raytracer/Render/Functor/RayTraceFunctor.cpp
@@ -11,27 +11,29 @@ RayTraceFunctor::RayTraceFunctor(
 
 void RayTraceFunctor::operator()(const MassiveCompute::Block& block)
 {
-    const size_t height = image.GetHeight();
+    const float aspectRatio = static_cast<float>(block.imageWidth) / static_cast<float>(block.imageHeight);
+    const float normalizedHalfViewWidth = aspectRatio;
+    const float normalizedHalfViewHeight = 1.f;
 
-    vec3<float> lowerLeftCorner = { -2.f, -1.f, -1.f };
-    vec3<float> horizontal = { 4.f, 0.f, 0.f };
-    vec3<float> vertical = { 0.f, 2.f, 0.f };
-    vec3<float> origin = { 0.f, 0.f, 0.f };
+    const vec3<float> lowerLeftCorner = { -normalizedHalfViewWidth, -normalizedHalfViewHeight, -1.f };
+    const vec3<float> horizontal = { normalizedHalfViewWidth * 2.f, 0.f, 0.f };
+    const vec3<float> vertical = { 0.f, normalizedHalfViewHeight * 2.f, 0.f };
+    const vec3<float> origin = { 0.f, 0.f, 0.f };
 
     for (size_t yRow = block.top; yRow < block.bottom; yRow++)
     {
         BGRA<uint8_t>* row = image.GetRow(yRow);
-        size_t y = block.imageHeight - 1 - yRow;
+        const size_t y = block.imageHeight - 1 - yRow;
 
         for (size_t x = block.left; x < block.right; x++)
         {
-            float u = static_cast<float>(x) / static_cast<float>(block.imageWidth);
-            float v = static_cast<float>(y) / static_cast<float>(block.imageHeight);
+            const float u = static_cast<float>(x) / static_cast<float>(block.imageWidth);
+            const float v = static_cast<float>(y) / static_cast<float>(block.imageHeight);
 
-            ray<float> r(origin, lowerLeftCorner + u * horizontal + v * vertical);
-            vec3<float> col = this->Color(r);
+            const ray<float> r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+            const vec3<float> col = this->Color(r);
 
-            vec3<float> col8Bit = col * 255.99f;
+            const vec3<float> col8Bit = col * 255.99f;
             BGRA<uint8_t> pixel = {};
 
             pixel.r = static_cast<uint8_t>(col8Bit.r);

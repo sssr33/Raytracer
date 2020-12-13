639bd6c17b895bb7946165e21bc7b0db45596c7e
Alexander Chaikovsky <sskr33@gmail.com>
2020-12-13 23:24:51 +0200

    [RT-8] comment about sphere equation simplification

--- Raytracer/Render/Hitable/Sphere.cpp
+++ Raytracer/Render/Hitable/Sphere.cpp
@@ -7,8 +7,27 @@ Sphere::Sphere(const vec3<float>& center, float radius)
 
 std::optional<HitRecord> Sphere::Hit(const ray<float>& ray, float tMin, float tMax) const
 {
-    vec3<float> fromSphereToRayOrigin = ray.origin - this->center;
+    /*
+    * Equation simplification
+    * float b = 2.f * ray.direction.dot(fromSphereToRayOrigin);
+    * x = ray.direction.dot(fromSphereToRayOrigin)
+    * 
+    * float b = 2.f * x;
+    * float discriminant = b * b - 4.f * a * c;
+    * float discriminant = 2.f * x * 2.f * x - 4.f * a * c;
+    * 
+    * float tClosest = (-b - sqrt(discriminant)) / (2.f * a);
+    * float tClosest = (-2.f * x - sqrt(2.f * x * 2.f * x - 4.f * a * c)) / (2.f * a);
+    * float tClosest = (-2.f * x - sqrt(4.f * x * x - 4.f * a * c)) / (2.f * a);
+    * float tClosest = (-2.f * x - sqrt(4.f * (x * x - a * c))) / (2.f * a);
+    * float tClosest = (-2.f * x - sqrt(4) * sqrt(x * x - a * c)) / (2.f * a);
+    * float tClosest = (-2.f * x - 2 * sqrt(x * x - a * c)) / (2.f * a);
+    * divide equation by 2 to simplify
+    * float tClosest = (-x - sqrt(x * x - a * c)) / a;
+    * float tClosest = -(x + sqrt(x * x - a * c)) / a;
+    */
 
+    vec3<float> fromSphereToRayOrigin = ray.origin - this->center;
     float a = ray.direction.dot(ray.direction);
     float b = ray.direction.dot(fromSphereToRayOrigin);
     float c = fromSphereToRayOrigin.dot(fromSphereToRayOrigin) - radius * radius;

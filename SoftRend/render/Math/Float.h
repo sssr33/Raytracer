5e9a4b37cb4bdb84b0fe854dcd853389d06a9615
Alexander Chaikovsky <sskr33@gmail.com>
2023-11-19 22:09:56 +0200

    default equality operators for Math/Float types

--- SoftRend/render/Math/Float.h
+++ SoftRend/render/Math/Float.h
@@ -7,6 +7,8 @@ struct Float4 {
     Float4(float x, float y, float z, float w)
         : x(x), y(y), z(z), w(w)
     {}
+
+    bool operator==(const Float4&) const = default;
 };
 
 struct Float3 {
@@ -16,6 +18,8 @@ struct Float3 {
     Float3(float x, float y, float z)
         : x(x), y(y), z(z)
     {}
+
+    bool operator==(const Float3&) const = default;
 };
 
 struct Float2 {
@@ -25,4 +29,6 @@ struct Float2 {
     Float2(float x, float y)
         : x(x), y(y)
     {}
+
+    bool operator==(const Float2&) const = default;
 };

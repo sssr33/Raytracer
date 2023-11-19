0b77cf460598042e03850dd2a1381788c28a6d2b
Alexander Chaikovsky <sskr33@gmail.com>
2023-11-19 20:16:32 +0200

    Pipeline2 stubs for stages, vertex shader stage impl

--- SoftRend/render/Pipeline/Pipeline2.h
+++ SoftRend/render/Pipeline/Pipeline2.h
@@ -3,14 +3,19 @@
 #include "Semantic\SemanticValues.h"
 #include "Semantic\SemanticValueHelpers.h"
 
+#include <vector>
 #include <type_traits>
 #include <Helpers\Meta\ArgType.h>
 
 template<typename TraitsT = Pipeline2Traits<>>
 class Pipeline2 {
-    using VSOperatorT = decltype(&TraitsT::VertexShaderType::operator());
-    using PSOperatorT = decltype(&TraitsT::PixelShaderType::operator());
-    using BlendOperatorT = decltype(&TraitsT::BlendType::operator());
+    using VSType = typename TraitsT::VertexShaderType;
+    using PSType = typename TraitsT::PixelShaderType;
+    using BlendType = typename TraitsT::BlendType;
+
+    using VSOperatorT = decltype(&VSType::operator());
+    using PSOperatorT = decltype(&PSType::operator());
+    using BlendOperatorT = decltype(&BlendType::operator());
 
 public:
     using VSInputType = std::decay_t<typename ArgType<VSOperatorT>::template n_type<0>>;
@@ -19,7 +24,34 @@ public:
     using PSInputType = std::decay_t<typename ArgType<PSOperatorT>::template n_type<0>>;
     using PSOutputType = std::decay_t<typename ArgType<PSOperatorT>::return_type>;
 
+    void SetVertexShader(VSType vertexShader);
+    void SetPixelShader(PSType pixelShader);
+    void SetBlendState(BlendType blendState);
+
+    /*
+    * InputAssemblerT must get triangle count and read triangle vertices as VSInputType
+    */
+    template<typename InputAssemblerT>
+    void Draw(const InputAssemblerT& inputAssembler);
+
 private:
+    struct VSOutputTriangle {
+        VSOutputType v[3];
+    };
+
+    template<typename InputAssemblerT>
+    void VertexShaderStage(const InputAssemblerT& inputAssembler);
+    void ClipSpaceClipStage();
+    void ClipSpaceToNdcStage();
+    void RasterizeStage();
+
+    VSType vertexShader;
+    PSType pixelShader;
+    BlendType blendState;
+
+    std::vector<VSOutputTriangle> vsOutTriangles;
+    std::vector<VSOutputTriangle> vsOutClippedTriangles;
+    std::vector<VSOutputTriangle> vsOutClippedNdcTriangles;
 };
 
 #include "Pipeline2.hpp"
--- SoftRend/render/Pipeline/Pipeline2.hpp
+++ SoftRend/render/Pipeline/Pipeline2.hpp
@@ -1,3 +1,61 @@
 #pragma once
 #include "Pipeline2.h"
 #include "Semantic\SemanticValuesLerp.h"
+
+template<typename TraitsT>
+void Pipeline2<TraitsT>::SetVertexShader(VSType vertexShader) {
+    this->vertexShader = std::move(vertexShader);
+}
+
+template<typename TraitsT>
+void Pipeline2<TraitsT>::SetPixelShader(PSType pixelShader) {
+    this->pixelShader = std::move(pixelShader);
+}
+
+template<typename TraitsT>
+void Pipeline2<TraitsT>::SetBlendState(BlendType blendState) {
+    this->blendState = std::move(blendState);
+}
+
+template<typename TraitsT>
+template<typename InputAssemblerT>
+void Pipeline2<TraitsT>::Draw(const InputAssemblerT& inputAssembler) {
+    this->VertexShaderStage(inputAssembler);
+    this->ClipSpaceClipStage();
+    this->ClipSpaceToNdcStage();
+    this->RasterizeStage();
+}
+
+template<typename TraitsT>
+template<typename InputAssemblerT>
+void Pipeline2<TraitsT>::VertexShaderStage(const InputAssemblerT& inputAssembler) {
+    const size_t triangleCount = inputAssembler.GetTriangleCount();
+
+    this->vsOutTriangles.clear();
+    this->vsOutTriangles.reserve(triangleCount);
+
+    for (size_t i = 0; i < triangleCount; ++i) {
+        this->vsOutTriangles.emplace_back();
+        VSOutputTriangle& outTri = vsOutTriangles.back();
+
+        for (size_t v = 0; v < std::size(outTri.v); ++v) {
+            const VSInputType& inputVertex = inputAssembler.GetTriangleVertex(i, v);
+            outTri.v[v] = this->vertexShader(inputVertex);
+        }
+    }
+}
+
+template<typename TraitsT>
+void Pipeline2<TraitsT>::ClipSpaceClipStage() {
+
+}
+
+template<typename TraitsT>
+void Pipeline2<TraitsT>::ClipSpaceToNdcStage() {
+
+}
+
+template<typename TraitsT>
+void Pipeline2<TraitsT>::RasterizeStage() {
+
+}
